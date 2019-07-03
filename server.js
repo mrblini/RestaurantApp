3bfbe342364d00709e4d1a08d0403c19a5108622
pablodiazt <pablodiaztr@gmail.com>
2019-07-03 13:28:55 -0500

    c

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 3005;
 
 // Basic route that sends the user first to the AJAX Page
 app.get("/", function(req, res) {
@@ -16,3 +16,4 @@ app.get("/", function(req, res) {
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
 });
+
